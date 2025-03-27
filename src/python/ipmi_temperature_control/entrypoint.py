5a5650f91a4003772d910abca7c49c6e49155f4d
Manuel <manuel@truenas.local>
2025-03-27 16:02:38 +0100

    test daemonize

--- src/python/ipmi_temperature_control/entrypoint.py
+++ src/python/ipmi_temperature_control/entrypoint.py
@@ -1,20 +1,20 @@
 import argparse
 import os
 import sys
+import time
 import yaml
 import logging
 
-from daemonize import Daemonize
 from .config import Main
 from .smartctl import ThreadExecutor
 from .ipmi import IPMI
 
 def _main():
-    while True:
-        print("test")
-
     args = parser_args(sys.argv[1:])
+
     config = read_yaml(args.config_file)
+
+    print("Args: %s" % args)
 
     log_level = logging.DEBUG if args.debug else logging.INFO
 
@@ -23,74 +23,82 @@ def _main():
 
     logger = logging.getLogger(__name__)
 
-    executors = []
-    for device in config.devices:
-        executor = ThreadExecutor(logger, device.path)
-        executors.append(executor)
-        executor.start()
+    while True:
+        executors = []
+        for device in config.devices:
+            executor = ThreadExecutor(logger, device.path)
+            executors.append(executor)
+            executor.start()
 
-    any_alive = any(list(map(is_thread_alive, executors)))
-    while any_alive:
         any_alive = any(list(map(is_thread_alive, executors)))
+        while any_alive:
+            any_alive = any(list(map(is_thread_alive, executors)))
 
-    smartctl_results = list(map(to_results, executors))
+        smartctl_results = list(map(to_results, executors))
 
-    target_fan_speed = config.default_speed
-    for smartctl_result in smartctl_results:
-        smartctl = smartctl_result.smartctl
-        device_name = smartctl_result.device.name
-        exit_status = smartctl_result.get_exit_status()
+        target_fan_speed = config.default_speed
+        for smartctl_result in smartctl_results:
+            smartctl = smartctl_result.smartctl
+            device_name = smartctl_result.device.name
+            exit_status = smartctl_result.get_exit_status()
 
-        if not (exit_status.is_device_open_error() or exit_status.is_cli_parse_error() or exit_status.is_disk_failing()):
-            current_temperature = smartctl_result.temperature.current
+            if not (
+                    exit_status.is_device_open_error() or exit_status.is_cli_parse_error() or exit_status.is_disk_failing()):
+                current_temperature = smartctl_result.temperature.current
 
-            logger.info("Current temperature of %s is %s C", device_name, current_temperature)
+                logger.info("Current temperature of %s is %s C", device_name, current_temperature)
 
-            for temperature_limit, fan_speed in config.fan_curve.items():
-                if current_temperature >= temperature_limit and target_fan_speed < fan_speed:
-                    logger.debug("Increasing target fan speed to %s %s due to %s exceeding temperature limit", fan_speed, "%", device_name)
-                    target_fan_speed = fan_speed
+                for temperature_limit, fan_speed in config.fan_curve.items():
+                    if current_temperature >= temperature_limit and target_fan_speed < fan_speed:
+                        logger.debug("Increasing target fan speed to %s %s due to %s exceeding temperature limit",
+                                     fan_speed, "%", device_name)
+                        target_fan_speed = fan_speed
 
-        if not exit_status.is_successful():
-            logger.warning("Got non 0 exit status (decimal: %d, binary: %s) for device (%s)",
-                           exit_status.decimal_value, exit_status.binary_value, device_name)
+            if not exit_status.is_successful():
+                logger.warning("Got non 0 exit status (decimal: %d, binary: %s) for device (%s)",
+                               exit_status.decimal_value, exit_status.binary_value, device_name)
 
-        if exit_status.has_test_errors():
-            logger.debug("The device self-test log contains records of errors. [ATA only] Failed self-tests outdated by a "
-                         "newer successful extended self-test are ignored.")
+            if exit_status.has_test_errors():
+                logger.debug(
+                    "The device self-test log contains records of errors. [ATA only] Failed self-tests outdated by a "
+                    "newer successful extended self-test are ignored.")
 
-        if exit_status.is_device_open_error():
-            logger.error("Could not read device info for %s", device_name)
-            logger.error(smartctl.messages[0].string)
-            continue
+            if exit_status.is_device_open_error():
+                logger.error("Could not read device info for %s", device_name)
+                logger.error(smartctl.messages[0].string)
+                continue
 
-    logger.info("Setting fan speed to %s %s", target_fan_speed, "%")
+        logger.info("Setting fan speed to %s %s", target_fan_speed, "%")
 
-    ipmi = IPMI(logger, args.host, args.username, args.password)
+        time.sleep(5)
 
-    fan_speed_hex_value = str(hex(target_fan_speed))
 
-    logger.debug("Converting fan speed to hex: %s", fan_speed_hex_value)
-    # noinspection PyListCreation
-    args = ["raw", "0x3a", "0x01"]
-    # CPU_FAN1
-    args.append("0x64")
-    # ?
-    args.append("0x64")
-    # ?
-    args.append("0x64")
-    # REAR_FAN2
-    args.append("0x64")
-    # FRNT_FAN1
-    args.append("0x64")
-    # FRNT_FAN2
-    args.append(fan_speed_hex_value)
-    # FRNT_FAN3
-    args.append(fan_speed_hex_value)
-    # ?
-    args.append("0x64")
 
-    ipmi.execute_set_fan_speed(args)
+    # ipmi = IPMI(logger, args.host, args.username, args.password)
+    #
+    # fan_speed_hex_value = str(hex(target_fan_speed))
+    #
+    # logger.debug("Converting fan speed to hex: %s", fan_speed_hex_value)
+    # # noinspection PyListCreation
+    # args = ["raw", "0x3a", "0x01"]
+    # # CPU_FAN1
+    # args.append("0x64")
+    # # ?
+    # args.append("0x64")
+    # # ?
+    # args.append("0x64")
+    # # REAR_FAN2
+    # args.append("0x64")
+    # # FRNT_FAN1
+    # args.append("0x64")
+    # # FRNT_FAN2
+    # args.append(fan_speed_hex_value)
+    # # FRNT_FAN3
+    # args.append(fan_speed_hex_value)
+    # # ?
+    # args.append("0x64")
+    #
+    # ipmi.execute_set_fan_speed(args)
 
 def is_thread_alive(thread: ThreadExecutor):
     return thread.is_alive()
@@ -121,6 +129,6 @@ def read_yaml(file_path: str):
     return Main(**yaml_file)
 
 def main():
-    pid = "/tmp/ipmi-temperature-control.pid"
-    daemon = Daemonize(app="ipmi-temperature-control", pid=pid, action=_main())
+    pid = "/tmp/test.pid"
+    daemon = Daemonize(app="ipmi-temperature-control", pid=pid, action=_main, foreground=True)
     daemon.start()
